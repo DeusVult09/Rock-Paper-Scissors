f56001f4e39415b0631eafce5c5efe3eff7ede75
Zarifa <mamadalievazara@gmail.com>
2025-07-25 14:25:51 +0200

    Fix: Remove  from getHumanChoice function to prevent the popup window ramain unclosed.

--- main.js
+++ main.js
@@ -19,8 +19,7 @@ function getHumanChoice() {
   const response = prompt(input);
   
   if (response === null) {
-    alert("Game canceled.");
-    return null;
+    alert("Game canceled!");
   }
 
   return response.toLowerCase();
